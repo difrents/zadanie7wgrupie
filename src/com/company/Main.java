d74e55af73826fd0fbf0cbf96ac2efe7c2b0c59b
Lukasz Hajduczenia <difrent.noone@tutanota.com>
2018-12-01 15:46:42 +0100

    Metody dodawanie i dzielenie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,36 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int a;
+
+
+
+
+        //dodawanie i dzielenie
 	// write your code here
     }
+
+    public static int dodawanie(){
+        System.out.println("Wybrałeś dodawanie"+"\n"+"Podaj dwie liczby, które chcesz do siebie dodać");
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner1.nextInt();
+
+        return a+b;
+    }
+
+    public static double dzielenie(){
+        System.out.println("Wybrałeś dzielenie"+"\n"+"Podaj dwie liczby, które chcesz podzielić(Najpierw dzielną, następnie dzielnik)");
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        double a = scanner.nextInt();
+        double b = scanner1.nextInt();
+
+        return a/b;
+    }
+
 }
